463601cd070ea07b31ce905b3e6447e7daab780f
fernandoareias <nando.calheirosx@gmail.com>
2021-09-01 12:20:37 -0300

    Command for Mark Todo AS Undone

---
+++ Todo.Domain/Commands/MarkTodoAsUndoneCommand.cs
@@ -0,0 +1,29 @@
+
+using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class MarkTodoAsUndoneCommand : Notifiable<Notification>, ICommands
+    {
+        public MarkTodoAsUndoneCommand() { }
+        public MarkTodoAsUndoneCommand(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string User { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract<Notification>()
+                .Requires()
+                .IsGreaterThan(User, 3, "User", "User should have at least 3 chars")
+           );
+        }
+    }
+}
