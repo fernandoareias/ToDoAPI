74339767f9284c2df29074931181c5ee7ca8e1d1
fernandoareias <nando.calheirosx@gmail.com>
2021-09-01 10:55:07 -0300

    Create Command

---
+++ Todo.Domain/Commands/CreateToDoCommands.cs
@@ -0,0 +1,32 @@
+
+
+using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class CreateTodoCommand : Notifiable<Notification>, ICommands
+    {
+        public CreateTodoCommand() { }
+        public CreateTodoCommand(string title, string user, DateTime date)
+        {
+            Title = title;
+            User = user;
+            Date = date;
+        }
+
+        public string Title { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract<Notification>()
+                .IsGreaterThan(Title, 3, "Title", "Title should have at least 3 chars")
+                .IsGreaterThan(User, 3, "User", "User should have at least 3 chars")
+           );
+        }
+    }
+}
